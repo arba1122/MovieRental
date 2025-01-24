ef50b49ecfd6c67c2985a2fb32c11290499383c8
arba1122 <arbanadauti@hotmail.com>
2025-01-24 21:19:54 +0100

    Initial commit

---
+++ Customer.cs
@@ -0,0 +1,7 @@
+public class Customer
+{
+    public int CustomerID { get; set; }
+    public string Name { get; set; }
+    public string ContactInfo { get; set; }
+    public string MembershipLevel { get; set; }
+}
---
+++ Rental.cs
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+public class Rental
+{
+    public int RentalID { get; set; }
+    public int CustomerID { get; set; }
+    public int MovieID { get; set; }
+    public DateTime RentalDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+}
