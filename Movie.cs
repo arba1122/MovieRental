f6f491eb2a8adc3037ef01ad7faeaf9b9a8d7975
arba1122 <arbanadauti@hotmail.com>
2025-01-24 21:18:34 +0100

    Added Movie class

---
+++ Movie.cs
@@ -0,0 +1,12 @@
+
+
+
+
+public class Movie
+{
+    public int MovieID { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public decimal RentalPrice { get; set; }
+}
